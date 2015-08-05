77d9f3a18429dfe865829cbbeac5636842629c80
Kamesh <kamesh.arumugam@gmail.com>
2015-08-05 15:08:23 +0530

    CrashReporter sample electron

---
+++ CrashReporter/server_stub_3000/README.md
@@ -0,0 +1,12 @@
+get-post-handle-express
+=======================
+
+Handle POST Request in Express 4
+
+Download code and run <code>node server.js</code>
+
+Vist <code>http://localhost:3000</code> to view the app.
+
+Watch the demo here : 
+
+[View the Youtube Demo](https://www.youtube.com/watch?v=C3G3N4LMJeE)
---
+++ CrashReporter/server_stub_3000/server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var formidable = require('formidable'),
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.post('/crashreporter', function(req, res) {
+    console.log(" crashreporter =============== START");
+    console.log(" Headers : ", req.headers);
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        console.log('Parsing formdata !!!');
+        console.log({
+            fields: fields,
+            files: files
+        });
+    });
+	console.log(" crashreporter =============== END");
+    res.end("DefaultResponse");
+});
+
+
+app.listen(3000, function() {
+    console.log("Started on PORT 3000");
+})
